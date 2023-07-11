cddd7f8ab9f4658a7903e0c69eb93bffe9d5aaf4
Serhat <100749817+ErzenSerhat@users.noreply.github.com>
2023-07-11 06:59:21 +0300

    Create redditbot.py

---
+++ redditbot.py
@@ -0,0 +1,31 @@
+import praw
+
+# Reddit API kimlik bilgileri
+reddit_client_id = "#"
+reddit_client_secret = "#"
+reddit_username = "#"
+reddit_password = "#"
+
+
+# Reddit API'ye bağlanma
+reddit = praw.Reddit(client_id=reddit_client_id,
+                     client_secret=reddit_client_secret,
+                     username=reddit_username,
+                     password=reddit_password,
+                     user_agent="Reddit Image Downloader")
+
+# İndirilecek subreddit ve post sayısı
+subreddit_name = "gamermemes"
+post_limit = 30
+
+# Reddit'ten görselleri indirme
+subreddit = reddit.subreddit(subreddit_name)
+posts = subreddit.hot(limit=post_limit)
+
+image_urls = []
+for post in posts:
+    if post.url.endswith((".jpg", ".jpeg", ".png")):
+        image_urls.append(post.url)
+
+# Indirilen görselleri Instagram'a yükleme
+print(image_urls)
